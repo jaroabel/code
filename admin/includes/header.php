1706ba1483e9f23fff80f5b6c338f40ec929e65a
jaroabel <jaroabel@gmail.com>
2020-03-05 21:41:29 -0500

    Fixed admin menu

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -27,16 +27,29 @@ require_once("admin_init.php");
     <title>JMABEL</title>
   </head>
   <body>
-<nav class="navbar navbar-dark bg-dark">
+
+<!-- Test new navbar -->
+<!--Navbar-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+  <!-- Navbar brand -->
   <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+  <!-- Collapse button -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
+    aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <!-- Collapsible content -->
+  <div class="collapse navbar-collapse" id="basicExampleNav">
+
+    <!-- Links -->
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#">Home
+          <span class="sr-only">(current)</span>
+        </a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="new_account.php">New Account</a>
@@ -44,27 +57,32 @@ require_once("admin_init.php");
       <li class="nav-item">
         <a class="nav-link" href="find_user.php">Users</a>
       </li>
+
+      <!-- Dropdown -->
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+          aria-haspopup="true" aria-expanded="false">Dropdown</a>
+        <div class="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
           <a class="dropdown-item" href="#">Action</a>
           <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+          <a class="dropdown-item" href="#">Logout</a>
         </div>
       </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
+
     </ul>
+    <!-- Links -->
+
     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
   </div>
+  <!-- Collapsible content -->
+
 </nav>
+<!--/.Navbar-->
+<!-- End test new navbar -->
+
 <p>&nbsp;</p>
 <!--- Stating container div -->
 <div class="container-fluid">
--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,6 @@
 <?php
 
-include('include/header.php');
+include('includes/header.php');
 
 ?>
 
